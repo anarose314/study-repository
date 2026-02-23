2e29c7b34fccd2179c38a2d15b87579c386b6a14
anarose314 <dpgp706@gmail.com>
2026-02-23 19:06:50 +0900

    feat(codeitmall): 파라미터 추가

--- nextjs-codeitmall/app/products/[id]/page.js
+++ nextjs-codeitmall/app/products/[id]/page.js
@@ -1,11 +1,14 @@
 import Button from '@/components/Button';
 import Productinfo from './components/Productinfo';
 
-export default function Product() {
+export default async function Product({ params }) {
+  const { id } = await params;
+
   return (
     <div>
       <Productinfo />
       <Button>구매하기</Button>
+      <div>상품 id : {id}</div>
     </div>
   );
 }
