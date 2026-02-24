7a380b0fa1b99aa111a8cb5f6a6d089f8a1e9b1b
anarose314 <dpgp706@gmail.com>
2026-02-24 10:41:37 +0900

    feat(codeitmall): 레이아웃 디자인 적용

--- nextjs-codeitmall/app/(product-list)/layout.js
+++ nextjs-codeitmall/app/(product-list)/layout.js
@@ -1,10 +1,11 @@
 import SearchFrom from './components/SearchFrom';
+import styles from './layout.module.css';
 
 export default function ProductListLayout({ children }) {
   return (
     <div>
       <SearchFrom />
-      <div style={{ marginTop: '16px' }}>{children}</div>
+      <div className={styles.container}>{children}</div>
     </div>
   );
 }
--- nextjs-codeitmall/components/Header.js
+++ nextjs-codeitmall/components/Header.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
-import styles from './Hedaer.module.css';
+import logoWhite from '@/assets/logo-white.svg';
+import settingWhite from '@/assets/setting-white.svg';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <>
-      <header className={styles.header}>
-        <Link href="/">홈</Link>
-        <Link href="/settings">설정</Link>
-      </header>
-    </>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Link href="/">
+          <img src={logoWhite.src} alt="CodeitMall" width={155} height={29} />
+        </Link>
+        <Link href="/settings">
+          <img src={settingWhite.src} alt="Settings" width={20} height={20} />
+        </Link>
+      </div>
+    </header>
   );
 }
