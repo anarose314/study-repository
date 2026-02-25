61f7a05e5ea37bd81aef51ffb7701bf5ad293ceb
anarose314 <dpgp706@gmail.com>
2026-02-25 17:45:02 +0900

    feat(codeitmall): Theme light / dark 디자인 적용

--- nextjs-codeitmall/contexts/ThemeContext.js
+++ nextjs-codeitmall/contexts/ThemeContext.js
@@ -1,11 +1,16 @@
 'use client';
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('dark');
+
+  useEffect(() => {
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
